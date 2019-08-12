14843d718c00a4287f46bbbe0dc3f2fe288f03b7
Rahul Kumar Jain <toniqapps@gmail.com>
2019-08-12 19:42:19 +0530

    Update titu_one_cycle_lr.py

--- titu_one_cycle_lr.py
+++ titu_one_cycle_lr.py
@@ -143,8 +143,8 @@ class OneCycleLR(Callback):
 
         self.epochs = self.params['epochs']
 	
-        if not self.batch_size:
-		self.batch_size = self.params['batch_size']
+        #if not self.batch_size:
+	#	self.batch_size = self.params['batch_size']
         #self.samples = self.params['samples']
         self.steps = self.params['steps']
 
